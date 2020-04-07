2258c4d973e186a00b89dd343e37d58ae7e10d0b
wjh <wjh@xyz.com>
2020-04-07 11:17:58 +0800

    JavaScript replace

---
+++ src/js-learn/js-string.js
@@ -0,0 +1,8 @@
+test1();
+
+function test1() {
+    console.log('test1');
+    var str = 'Take collaboration to the next level with security and administrative features built for teams.';
+    var replace = str.replace('collaboration', '');
+    console.log(replace);
+}
