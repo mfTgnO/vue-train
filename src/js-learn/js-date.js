092e09a09e9cc5c22f57c3931c22f5f48f21c861
wjh <wjh@xyz.com>
2020-09-17 14:13:08 +0800

    JavaScript Date

--- src/js-learn/js-date.js
+++ src/js-learn/js-date.js
@@ -1,19 +1,142 @@
-// test1();
-test2();
+/**
+ * JavaScript Date objects represent a single moment in time in a platform-independent format.
+ * Date objects contain a Number that represents milliseconds since 1 January 1970 UTC.
+ *
+ * TC39 is working on Temporal, a new Date/Time API.
+ * Read more about it on the Igalia blog and fill out the survey. It needs real-world feedback from web developers, but is not yet ready for production use!
+ *
+ * Description
+ * The ECMAScript epoch and timestamps
+ * A JavaScript date is fundamentally specified as the number of milliseconds that have elapsed since midnight on January 1, 1970, UTC.
+ * This date and time is the same as the UNIX epoch, which is the predominant base value for computer-recorded date and time values.
+ *
+ * Note: It's important to keep in mind that while the time value at the heart of a Date object is UTC,
+ * the basic methods to fetch the date and time or its components all work in the local (i.e. host system) time zone and offset.
+ *
+ * It should be noted that the maximum Date is not of the same value as the maximum safe integer (Number.MAX_SAFE_INTEGER is 9,007,199,254,740,991).
+ * Instead, it is defined in ECMA-262 that a maximum of ±100,000,000 (one hundred million) days relative to January 1, 1970 UTC
+ * (that is, April 20, 271821 BCE ~ September 13, 275760 CE) can be represented by the standard Date object (equivalent to ±8,640,000,000,000,000 milliseconds).
+ *
+ * Date format and time zone conversions
+ * There are a number of methods available to obtain a date in various formats, as well as to perform time zone conversions.
+ * Particularly useful are the functions that output the date and time in Coordinated Universal Time (UTC), the global standard
+ * time defined by the World Time Standard. (This time is historically known as Greenwich Mean Time, as UTC lies along the meridian
+ * that includes London—and nearby Greenwich—in the United Kingdom.) The user's device provides the local time.
+ *
+ * In addition to methods to read and alter individual components of the local date and time (such as getDay() and setHours()),
+ * there are also versions of the same methods that read and manipulate the date and time using UTC (such as getUTCDay() and setUTCHours()).
+ */
+test1();
+// test2();
+// test3();
 
 /**
- * Date
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+ * Date.now()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+ *
+ * The static Date.now() method returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC.
+ *
+ * Syntax
+ * var timeInMs = Date.now();
+ *
+ * Return value
+ * A Number representing the milliseconds elapsed since the UNIX epoch.
  */
 function test1() {
-    var date = new Date();
-    console.log(date);
+    // this example takes 2 seconds to run
+    const start = Date.now();
 
-    var date2 = Date.now();
-    console.log(date2);
+    console.log('starting timer...');
+    console.log(start);
+    // expected output: starting timer...
+
+    setTimeout(() => {
+        const millis = Date.now() - start;
+
+        console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+        // expected output: seconds elapsed = 2
+    }, 2000);
 }
 
 function test2() {
     var date = new Date().toLocaleString();
     console.log(date);
+}
+
+/**
+ * Date.UTC()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/UTC
+ *
+ * The Date.UTC() method accepts parameters similar to the Date constructor, but treats them as UTC.
+ * It returns the number of milliseconds since January 1, 1970, 00:00:00 UTC.
+ *
+ * Syntax
+ * Since ECMAScript 2017:
+ * Date.UTC(year[, month[, day[, hour[, minute[, second[, millisecond]]]]]])
+ *
+ * ECMAScript 2016 and earlier: (month used to be required)
+ * Date.UTC(year, month[, day[, hour[, minute[, second[, millisecond]]]]])
+ *
+ * Parameters
+ * year
+ * A full year.
+ *
+ * month
+ * An integer between 0 (January) and 11 (December) representing the month. (Up through ECMAScript 2016, month was a required parameter. As of ES2017, it no longer is.)
+ *
+ * day Optional
+ * An integer between 1 and 31 representing the day of the month. If omitted, defaults to 1.
+ *
+ * hour Optional
+ * An integer between 0 and 23 representing the hours. If omitted, defaults to 0.
+ *
+ * minute Optional
+ * An integer between 0 and 59 representing the minutes. If omitted, defaults to 0.
+ *
+ * second Optional
+ * An integer between 0 and 59 representing the seconds. If omitted, defaults to 0.
+ *
+ * millisecond Optional
+ * An integer between 0 and 999 representing the milliseconds. If omitted, defaults to 0.
+ *
+ * Return value
+ * A number representing the number of milliseconds for the given date since January 1, 1970, 00:00:00, UTC.
+ *
+ * Description
+ * UTC() takes comma-delimited date and time parameters and returns the number of milliseconds between January 1, 1970, 00:00:00,
+ * universal time and the specified date and time.
+ * Years between 0 and 99 are converted to a year in the 20th century (1900 + year). For example, 95 is converted to the year 1995.
+ *
+ * The UTC() method differs from the Date constructor in two ways:
+ *      1.Date.UTC() uses universal time instead of the local time.
+ *      2.Date.UTC() returns a time value as a number instead of creating a Date object.
+ *
+ * If a parameter is outside of the expected range, the UTC() method updates the other parameters to accommodate the value.
+ * For example, if 15 is used for month, the year will be incremented by 1 (year + 1) and 3 will be used for the month.
+ * UTC() is a static method of Date, so it's called as Date.UTC() rather than as a method of a Date instance.
+ */
+function test3() {
+    const utcDate1 = new Date(Date.UTC(96, 1, 2, 3, 4, 5));
+    const utcDate2 = new Date(Date.UTC(0, 0, 0, 0, 0, 0));
+
+    console.log(utcDate1.toUTCString());
+    // expected output: Fri, 02 Feb 1996 03:04:05 GMT
+
+    console.log(utcDate2.toUTCString());
+    // expected output: Sun, 31 Dec 1899 00:00:00 GMT
+}
+
+/**
+ * function test3() {
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+ *
+ * The Date.parse() method parses a string representation of a date, and returns the number of milliseconds since
+ * January 1, 1970, 00:00:00 UTC or NaN if the string is unrecognized or, in some cases, contains illegal date values (e.g. 2015-02-31).
+ *
+ * It is not recommended to use Date.parse as until ES5, parsing of strings was entirely implementation dependent.
+ * There are still many differences in how different hosts parse date strings, therefore date strings should be
+ * manually parsed (a library can help if many different formats are to be accommodated).
+ */
+function test4() {
+
 }
--- src/js-learn/js-map.js
+++ src/js-learn/js-map.js
@@ -21,7 +21,15 @@
  * However, there are important differences that make Map preferable in certain cases:
  */
 // test1();
-test2();
+// test2();
+// test3();
+// test4();
+// test5();
+// test6();
+// test7();
+// test8();
+// test9();
+test10();
 
 /**
  * Map.prototype.set()
@@ -78,4 +86,245 @@ function test2() {
     map.set('bar', 'foo');
 
     console.log(map.get('bar'));
+}
+
+/**
+ * Map.prototype.clear()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/clear
+ *
+ * The clear() method removes all elements from a Map object.
+ */
+function test3() {
+    var map = new Map();
+    map.set('foo', 1);
+    map.set('bar', 2);
+
+    console.log(map.size);
+    // expected output: 2
+
+    map.clear();
+
+    console.log(map.size);
+    // expected output: 0
+}
+
+/**
+ * Map.prototype.delete()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/delete
+ *
+ * The delete() method removes the specified element from a Map object by key.
+ *
+ * Syntax
+ * myMap.delete(key);
+ *
+ * Parameters
+ * key
+ * The key of the element to remove from the Map object.
+ *
+ * Return value
+ * true if an element in the Map object existed and has been removed, or false if the element does not exist.
+ */
+function test4() {
+    var map = new Map();
+    map.set('foo', 1);
+
+    console.log(map.delete('foo'));
+    // expected output: true
+    console.log(map.has('foo'));
+    // expected output: false
+    console.log(map.delete('foo'));
+    // expected output: false
+}
+
+/**
+ * Map.prototype.has()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/has
+ *
+ * The has() method returns a boolean indicating whether an element with the specified key exists or not.
+ */
+function test5() {
+    var map = new Map();
+    map.set('foo', 'bar');
+
+    console.log(map.has('foo'));
+    // expected output: true
+    console.log(map.has('fzz'));
+    // expected output: false
+}
+
+/**
+ * Map.prototype.entries()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/entries
+ *
+ * The entries() method returns a new Iterator object that contains the [key, value] pairs for each element in the Map object in insertion order.
+ */
+function test6() {
+    var map = new Map();
+    map.set(1, 'foo');
+    map.set(2, 'bar');
+
+    var iterator = map.entries();
+
+    console.log(iterator.next().value);
+    console.log(iterator.next().value);
+}
+
+/**
+ * Map.prototype.forEach()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/forEach
+ *
+ * The forEach() method executes a provided function once per each key/value pair in the Map object, in insertion order.
+ *
+ * Syntax
+ * myMap.forEach(callback([value][,key][,map])[, thisArg])
+ *
+ * Parameters
+ * callback
+ * Function to execute for each entry of myMap. It takes the following arguments:
+ *      value Optional
+ *          Value of each iteration.
+ *      key Optional
+ *          Key of each iteration.
+ *      map Optional
+ *          The map being iterated (myMap in the above Syntax box).
+ *
+ * thisArg Optional
+ * Value to use as this when executing callback.
+ *
+ * Description
+ * The forEach method executes the provided callback once for each key of the map which actually exist.
+ * It is not invoked for keys which have been deleted. However, it is executed for values which are present but have the value undefined.
+ *
+ * callback is invoked with three arguments:
+ *
+ * the entry's value
+ * the entry's key
+ * the Map object being traversed
+ * If a thisArg parameter is provided to forEach, it will be passed to callback when invoked,
+ * for use as its this value.  Otherwise, the value undefined will be passed for use as its this value.
+ * The this value ultimately observable by callback is determined according to the usual rules for determining the this seen by a function.
+ *
+ * Each value is visited once, except in the case when it was deleted and re-added before forEach has finished.
+ * callback is not invoked for values deleted before being visited. New values added before forEach has finished will be visited.
+ *
+ * forEach executes the callback function once for each element in the Map object. It does not return a value.
+ */
+function test7() {
+    function logMapElements(value, key, map) {
+        console.log(`m[${key}] = ${value}`);
+    }
+
+    new Map([['foo', 3], ['bar', {}], ['baz', undefined]])
+        .forEach(logMapElements);
+
+    // expected output: "m[foo] = 3"
+    // expected output: "m[bar] = [object Object]"
+    // expected output: "m[baz] = undefined"
+}
+
+/**
+ * Map.prototype.keys()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/keys
+ *
+ * The keys() method returns a new Iterator object that contains the keys for each element in the Map object in insertion order
+ *
+ * Syntax
+ * myMap.keys()
+ *
+ * Return value
+ * A new Map iterator object.
+ */
+function test8() {
+    var map = new Map();
+    map.set(1, 'foo');
+    map.set(2, 'bar');
+
+    var iterator = map.keys();
+    console.log(iterator.next().value);
+    console.log(iterator.next().value);
+}
+
+/**
+ * Map.prototype.values()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/values
+ *
+ * The values() method returns a new Iterator object that contains the values for each element in the Map object in insertion order.
+ *
+ * Syntax
+ * myMap.values()
+ *
+ * Return value
+ * A new Map iterator object.
+ */
+function test9() {
+    var map = new Map();
+    map.set(1, 'foo');
+    map.set(2, 'bar');
+
+    var iterator = map.values();
+    console.log(iterator.next().value);
+    console.log(iterator.next().value);
+}
+
+/**
+ * Map.prototype[@@iterator]()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/@@iterator
+ *
+ * The initial value of the @@iterator property is the same function object as the initial value of the entries method.
+ */
+function test10() {
+    // test10_1();
+    // test10_2();
+    test10_3();
+}
+
+function test10_1() {
+    var map = new Map();
+    map.set(1, 'foo');
+    map.set(2, 'bar');
+
+    var iterator = map[Symbol.iterator]();
+    for (const item of iterator) {
+        console.log(item);
+    }
+}
+
+/**
+ * Using [@@iterator]()
+ */
+function test10_2() {
+    const myMap = new Map();
+    myMap.set('0', 'foo');
+    myMap.set(1, 'bar');
+    myMap.set({}, 'baz');
+
+    const mapIter = myMap[Symbol.iterator]();
+
+    console.log(mapIter.next().value); // ["0", "foo"]
+    console.log(mapIter.next().value); // [1, "bar"]
+    console.log(mapIter.next().value); // [Object, "baz"]
+}
+
+/**
+ * Using [@@iterator]() with for..of
+ */
+function test10_3() {
+    const myMap = new Map()
+    myMap.set('0', 'foo')
+    myMap.set(1, 'bar')
+    myMap.set({}, 'baz')
+
+    for (const entry of myMap) {
+        console.log(entry)
+    }
+    // ["0", "foo"]
+    // [1, "bar"]
+    // [{}, "baz"]
+
+    for (const [key, value] of myMap) {
+        console.log(`${key}: ${value}`)
+    }
+    // 0: foo
+    // 1: bar
+    // [Object]: baz
 }
