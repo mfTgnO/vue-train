bb69c2777b298f3b542a42a95682914ce863c3f7
wjh <wjh@xyz.com>
2020-11-02 16:56:15 +0800

    The arguments object

--- src/js-learn/function/js-the-arguments-object.js
+++ src/js-learn/function/js-the-arguments-object.js
@@ -75,7 +75,9 @@ function test2_2() {
  * Defining a function that concatenates several strings
  */
 function test3() {
-    test3_1();
+    // test3_1();
+    // test3_2();
+    test3_3()
 }
 
 /**
@@ -101,4 +103,133 @@ function test3_1() {
     let x3 = myConcat('. ', 'sage', 'basil', 'oregano', 'pepper', 'parsley');
     console.log(x3);
     // sage. basil. oregano. pepper. parsley
+}
+
+/**
+ * Defining a function that creates HTML lists
+ * This example defines a function that creates a string containing HTML for a list. The only formal argument
+ * for the function is a string that is "u" if the list is to be unordered (bulleted), or "o" if the list is
+ * to be ordered (numbered). The function is defined as follows:
+ */
+function test3_2() {
+    function list(type) {
+        let html = '<' + type + 'l><li>';
+        var args = Array.prototype.slice.call(arguments, 1);
+        html += args.join('</li><li>');
+        html += '</li></' + type + 'l>';// end list
+        return html;
+    }
+
+    // You can pass any number of arguments to this function, and it adds each argument as a
+    // list item to a list of the type indicated. For example:
+    let listHTML = list('u', 'One', 'Two', 'Three');
+    console.log(listHTML);
+    // <ul><li>One</li><li>Two</li><li>Three</li></ul>
+}
+
+/**
+ * Rest, default, and destructured parameters
+ */
+function test3_3() {
+    // test3_3_1();
+    // test3_3_2();
+    // test3_3_3();
+    // test3_3_4();
+    // test3_3_5();
+    test3_3_6();
+}
+
+/**
+ * The arguments object can be used in conjunction with rest, default, and destructured parameters.
+ */
+function test3_3_1() {
+    function foo(...args) {
+        return args;
+    }
+
+    var x = foo(1, 2, 3);
+    console.log(x);
+    // [ 1, 2, 3 ]
+}
+
+/**
+ * While the presence of rest, default, or destructured parameters does not alter the behavior of the
+ * arguments object in strict mode code, there are subtle differences for non-strict code.
+ *
+ * In strict-mode code, the arguments object behaves the same whether or not a function is passed rest,
+ * default, or destructured parameters. That is, assigning new values to variables in the body of the
+ * function will not affect the arguments object. Nor will assigning new variables to the arguments
+ * object affect the value of variables.
+ *
+ * Note: You cannot write a "use strict"; directive in the body of a function definition that accepts rest,
+ * default, or destructured parameters. Doing so will throw a syntax error.
+ *
+ * Non-strict functions that are passed only simple parameters (that is, not rest, default, or restructured
+ * parameters) will sync the value of variables new values in the body of the function with the arguments object,
+ * and vice versa:
+ */
+function test3_3_2() {
+    function func(a) {
+        arguments[0] = 99;// // updating arguments[0] also updates a
+        console.log(a);
+    }
+
+    func(10);
+    // 99
+}
+
+/**
+ * And also:
+ */
+function test3_3_3() {
+    function func(a) {
+        a = 99;// updating a also updates arguments[0]
+        console.log(arguments[0]);
+    }
+
+    func(10);
+    // 99
+}
+
+/**
+ * Conversely, non-strict functions that are passed rest, default, or destructured parameters will not sync
+ * new values assigned to argument variables in the function body with the arguments object. Instead, the
+ * arguments object in non-strict functions with complex parameters will always reflect the values passed
+ * to the function when the function was called (this is the same behavior as exhibited by all strict-mode
+ * functions, regardless of the type of variables they are passed):
+ */
+function test3_3_4() {
+    function func(a = 55) {
+        arguments[0] = 99;// updating arguments[0] does not also update a
+        console.log(a);
+    }
+
+    func(10);
+    // 10
+}
+
+/**
+ * And also:
+ */
+function test3_3_5() {
+    function func(a = 55) {
+        a = 99;// updating a does not also update arguments[0]
+        console.log(arguments[0]);
+    }
+
+    func(10);
+    // 10
+}
+
+/**
+ * And also:
+ */
+function test3_3_6() {
+    // An untracked default parameter
+    function func(a = 55) {
+        console.log(arguments[0]);
+    }
+
+    func();
+    // undefined
 }
